a4ffe5ba018d3a95837bc43370d6f2181b547eac
mmamica <maria.anna.mamica@gmail.com>
2020-05-06 15:27:27 +0200

    CNN_preprocessing: adding full preprocessing and cnn models

--- CNN/scripts/eeg_cnn_lib.py
+++ CNN/scripts/eeg_cnn_lib.py
@@ -1,12 +1,13 @@
 import numpy as np
-from scipy.interpolate import griddata,CloughTocher2DInterpolator
-from sklearn.preprocessing import scale, normalize, MinMaxScaler
+from scipy.interpolate import CloughTocher2DInterpolator
+from sklearn.preprocessing import MinMaxScaler
 import math as m
-from utils import cart2sph, pol2cart
+import utils
+import cv2
 
 def azim_proj(pos):
-    [r, elev, az] = cart2sph(pos[0], pos[1], pos[2])
-    return pol2cart(az, m.pi / 2 - elev)
+    [r, elev, az] = utils.cart2sph(pos[0], pos[1], pos[2])
+    return utils.pol2cart(az, m.pi / 2 - elev)
 
 def gen_image(coordinates, values, channels, bands):
     output = np.zeros((channels,channels,bands))
@@ -20,4 +21,45 @@ def gen_image(coordinates, values, channels, bands):
         scaler.fit(interpolated)
         scaled = scaler.transform(interpolated)
         output[:,:,i] = scaled
+    output = 255 * output # Now scale by 255
+    output = output.astype(np.uint8)
     return output
+
+def gen_data(data, ratings, coordinates_2d, participant, category, ratio, fs, channels, window, path):
+    trials = data.shape[0]
+    time = int(data.shape[2]/fs)
+    np.random.seed()
+    for t in range(trials):
+        for s in range(0,time-int(window/2),int(window/2)):
+            thetas = np.zeros((channels,1))
+            alphas = np.zeros((channels,1))
+            betas = np.zeros((channels,1))
+            gammas = np.zeros((channels,1))
+            for i in range(channels):
+                data_temp = data[t][i][fs*s-1:fs*(s+window)]
+                theta, alpha, beta, gamma = utils.psd_bands(data_temp, fs)
+                alphas[i] = alpha
+                betas[i] = beta
+                gammas[i] = gamma
+                thetas[i] = theta
+            features = np.hstack((thetas, alphas, betas, gammas))
+            image = gen_image(coordinates_2d,features,channels,4)
+            path_name = path + "/" + category
+            if(np.random.rand()<=ratio):
+                path_name = path_name + "/train"
+            else:
+                path_name = path_name + "/test"
+            if(category == "valence"):
+                value = ratings[t,2]
+                if(value<5):
+                    path_name = path_name + "/low"
+                else:
+                    path_name = path_name + "/high"
+            if(category == "arousal"):
+                value = ratings[t,3]
+                if(value<5):
+                    path_name = path_name + "/low"
+                else:
+                    path_name = path_name + "/high"
+            path_name = path_name+"/"+str(participant)+str(t)+str(s)+".png"
+            cv2.imwrite(path_name,image)
